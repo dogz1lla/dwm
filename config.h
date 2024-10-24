1c0e9ab849dd45dace48f7b0426b7d6ecbe6d30a
dogz1lla <dogz1lla@arch.xyz>
2024-10-25 00:11:54 +0200

    greek letters as tags

--- config.h
+++ config.h
@@ -33,7 +33,8 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+/*static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };*/
+static const char *tags[] = { "\u03b1", "\u03b2", "\u03b3", "\u03b4" };
 
 static const Rule rules[] = {
 	/* xprop(1):
